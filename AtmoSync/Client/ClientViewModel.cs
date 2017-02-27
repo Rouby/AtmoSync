6224d863921952d3d1725dcbed849936e30b1caf
Jonathan Burke <jonathan.burke.1311@gmail.com>
2017-02-27 22:09:15 +0100

    Make it store-ready

--- AtmoSync/Client/ClientViewModel.cs
+++ AtmoSync/Client/ClientViewModel.cs
@@ -25,7 +25,6 @@ namespace AtmoSync.Client
         {
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             LoadSettingsAsync();
-            _soundFiles.Add(new Sound { ClientName = "asd" });
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
 
